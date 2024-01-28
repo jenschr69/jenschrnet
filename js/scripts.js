4a4147741387cb101d3af9c312095fe16fcdd455
Jens Chr. Christensen <jcc@jenschrchristensen.com>
2024-01-28 15:34:16 +0100

    Adding files

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,9 @@
+// Adding the Google Tag Manager Script to the head-tag and after the opening body tag
+
 // Add script for inserting page id
 
 // Add script for inserting global navigation
 
-// Assign classes for navigation to mark which (sub) page the user is on
-// Adding aria classes for this
+// Assign classes for navigation to mark which (sub) page the user is on using the class w3-teal?
+
+// Adding aria classes for the current page
