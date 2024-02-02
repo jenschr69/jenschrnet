436dbf21c597113cec3025ffbfaef0ed2587436b
Jens Chr. Christensen <jcc@jenschrchristensen.com>
2024-02-02 14:03:10 +0100

    Adding main.js reference to files

---
+++ modules/globalnav.js
@@ -0,0 +1,11 @@
+
+<a href="./" class="w3-bar-item w3-button w3-teal">Home</a>
+<a href="./full-cv/" class="w3-bar-item w3-button">Full CV</a>
+<a href="./education/" class="w3-bar-item w3-button">Education</a>
+<a href="./work-experience/" class="w3-bar-item w3-button">Work Experience</a>
+<a href="./courses/" class="w3-bar-item w3-button">Courses</a>
+<a href="./skills/" class="w3-bar-item w3-button">Skills</a>
+<a href="./about-me/" class="w3-bar-item w3-button">About me</a>
+<a href="./contact/" class="w3-bar-item w3-button">Contact</a>
+
+export globalnav 
