c9fddf83bf1df55bd5b225b15bb731f7265bbcb3
Jens Chr. Christensen <jcc@jenschrchristensen.com>
2024-02-02 14:18:56 +0100

    Adding export function and import function of nav module

---
+++ main.js
@@ -0,0 +1,4 @@
+// Import global navigation from modules/globalnav.js
+import {globalnav} from './modules/globalnav.js';
+
+// Add globalnav to nav-container
--- modules/globalnav.js
+++ modules/globalnav.js
@@ -1,4 +1,5 @@
-
+function globalnav() {
+// Create command for adding nav html to nav tag
 <a href="./" class="w3-bar-item w3-button w3-teal">Home</a>
 <a href="./full-cv/" class="w3-bar-item w3-button">Full CV</a>
 <a href="./education/" class="w3-bar-item w3-button">Education</a>
@@ -7,5 +8,6 @@
 <a href="./skills/" class="w3-bar-item w3-button">Skills</a>
 <a href="./about-me/" class="w3-bar-item w3-button">About me</a>
 <a href="./contact/" class="w3-bar-item w3-button">Contact</a>
+}
 
-export globalnav 
+export {globalnav};
